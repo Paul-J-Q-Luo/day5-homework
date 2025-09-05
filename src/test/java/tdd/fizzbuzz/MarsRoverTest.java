5936601a85ca66cf336b560cc7577f0be8c2c3ea
Paul J Q Luo <paul.j.q.luo@oocl.com>
2025-09-05 16:05:14 +0800

    test: update test method names to include x and y coordinates for clarity

--- src/test/java/tdd/fizzbuzz/MarsRoverTest.java
+++ src/test/java/tdd/fizzbuzz/MarsRoverTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MarsRoverTest {
 
     @Test
-    public void should_return_location_is_W_when_given_direction_is_N_and_command_is_L_and_x_is_0_and_y_is_0() {
+    public void should_return_location_is_W_and_x_is_0_and_y_is_0_when_given_direction_is_N_and_command_is_L_and_x_is_0_and_y_is_0() {
         String direction = "N";
         String command = "L";
         int x = 0;
@@ -22,7 +22,7 @@ class MarsRoverTest {
     }
 
     @Test
-    public void should_return_location_is_W_when_given_direction_is_N_and_command_is_R_and_x_is_0_and_y_is_0() {
+    public void should_return_location_is_W_and_x_is_0_and_y_is_0_when_given_direction_is_N_and_command_is_R_and_x_is_0_and_y_is_0() {
         String direction = "N";
         String command = "R";
         int x = 0;
@@ -37,7 +37,7 @@ class MarsRoverTest {
     }
 
     @Test
-    public void should_return_location_is_E_when_given_direction_is_S_and_command_is_L_and_x_is_0_and_y_is_0() {
+    public void should_return_location_is_E_and_x_is_0_and_y_is_0_when_given_direction_is_S_and_command_is_L_and_x_is_0_and_y_is_0() {
         String direction = "S";
         String command = "L";
         int x = 0;
@@ -52,7 +52,7 @@ class MarsRoverTest {
     }
 
     @Test
-    public void should_return_location_is_W_when_given_direction_is_S_and_command_is_R_and_x_is_0_and_y_is_0() {
+    public void should_return_location_is_W_and_x_is_0_and_y_is_0_when_given_direction_is_S_and_command_is_R_and_x_is_0_and_y_is_0() {
         String direction = "S";
         String command = "R";
         int x = 0;
@@ -67,7 +67,7 @@ class MarsRoverTest {
     }
 
     @Test
-    public void should_return_location_is_N_when_given_direction_is_E_and_command_is_L_and_x_is_0_and_y_is_0() {
+    public void should_return_location_is_N_and_x_is_0_and_y_is_0_when_given_direction_is_E_and_command_is_L_and_x_is_0_and_y_is_0() {
         String direction = "E";
         String command = "L";
         int x = 0;
@@ -82,7 +82,7 @@ class MarsRoverTest {
     }
 
     @Test
-    public void should_return_location_is_S_when_given_direction_is_E_and_command_is_R_and_x_is_0_and_y_is_0() {
+    public void should_return_location_is_S_and_x_is_0_and_y_is_0_when_given_direction_is_E_and_command_is_R_and_x_is_0_and_y_is_0() {
         String direction = "E";
         String command = "R";
         int x = 0;
@@ -97,7 +97,7 @@ class MarsRoverTest {
     }
 
     @Test
-    public void should_return_location_is_S_when_given_direction_is_W_and_command_is_L_and_x_is_0_and_y_is_0() {
+    public void should_return_location_is_S_and_x_is_0_and_y_is_0_when_given_direction_is_W_and_command_is_L_and_x_is_0_and_y_is_0() {
         String direction = "W";
         String command = "L";
         int x = 0;
@@ -112,7 +112,7 @@ class MarsRoverTest {
     }
 
     @Test
-    public void should_return_location_is_N_when_given_direction_is_W_and_command_is_R_and_x_is_0_and_y_is_0() {
+    public void should_return_location_is_N_and_x_is_0_and_y_is_0_when_given_direction_is_W_and_command_is_R_and_x_is_0_and_y_is_0() {
         String direction = "W";
         String command = "R";
         int x = 0;
