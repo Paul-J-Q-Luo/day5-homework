3bdf1f815521a4b9c917e2de667faeecb2af99eb
Paul J Q Luo <paul.j.q.luo@oocl.com>
2025-09-05 11:07:13 +0800

    feat: implement countOff method in FizzBuzz class and add unit tests

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,37 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_1_when_input_1() {
+        int order = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.countOff(order);
+
+        assertEquals("1", result);
+    }
+
+    @Test
+    public void should_return_2_when_input_2() {
+        int order = 2;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.countOff(order);
+
+        assertEquals("2", result);
+    }
+
+    @Test
+    public void should_return_Fizz_when_input_3() {
+        int order = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.countOff(order);
+
+        assertEquals("Fizz", result);
+    }
 }
