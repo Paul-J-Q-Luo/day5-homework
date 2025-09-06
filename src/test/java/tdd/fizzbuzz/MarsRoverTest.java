439903cd310c4276dc53dc7c4b808ee03673ba79
Paul J Q Luo <paul.j.q.luo@oocl.com>
2025-09-06 13:00:54 +0800

    test: add test for backward movement in northern direction

--- src/test/java/tdd/fizzbuzz/MarsRoverTest.java
+++ src/test/java/tdd/fizzbuzz/MarsRoverTest.java
@@ -185,4 +185,19 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getY());
         assertEquals("W", marsRover.getDirection());
     }
+
+    @Test
+    public void should_return_location_is_N_and_x_is_0_and_y_is_nav_1_when_given_direction_is_N_and_command_is_B_and_x_is_0_and_y_is_0() {
+        String direction = "N";
+        String command = "B";
+        int x = 0;
+        int y = 0;
+        MarsRover marsRover = new MarsRover(x, y, direction);
+
+        marsRover.executeCommand(command);
+
+        assertEquals(0, marsRover.getX());
+        assertEquals(-1, marsRover.getY());
+        assertEquals("N", marsRover.getDirection());
+    }
 }
